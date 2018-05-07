63d782b3b2584dae445abe6e03690822624f7625
Kristof Kessler <kristof.kessler@tu-braunschweig.de>
2018-05-07 14:28:44 +0200

    Ticket #2250

--- themes/daia/js/daia.js
+++ themes/daia/js/daia.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	$('.daia-availability').each(function(){
 		var $this = $(this);
         $.ajax({
-			url:'/vufind/PAIA/availability?ppn='+jQuery(this).attr('data-ppn')+'&daia='+jQuery(this).data('daia')+'&openurl='+jQuery(this).data('openurl')+'&marcField951aValue='+jQuery(this).attr('data-marcField951aValue')+'&requesturi='+jQuery(this).data('requesturi')+'&list='+jQuery(this).data('list')+'&usedaia='+jQuery(this).data('usedaia'),
-			//url:'/services/availability/templates/availability.phtml?ppn='+$(this).attr('data-ppn')+'&list='+$(this).attr('data-list')+'&site='+$(this).attr('data-site'),
+			url:'/vufind/PAIA/availability?ppn='+jQuery(this).attr('data-ppn')+'&daia='+jQuery(this).data('daia')+'&openurl='+jQuery(this).data('openurl')+'&marcField951aValue='+jQuery(this).attr('data-marcField951aValue')+'&requesturi='+jQuery(this).data('requesturi')+'&list='+jQuery(this).data('list')+'&usedaia='+jQuery(this).data('usedaia')+'&format='+jQuery(this).data('format'),
 			success:function(data) {
 				$this.html(jQuery(data).find('#paia-availability'));
 			}
