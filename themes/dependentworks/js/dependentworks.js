e7a4287313aaa39af1ff6beb1bdfa954db87a9d8
hajoseng <hajo.seng@sub.uni-hamburg.de>
2018-07-03 13:13:19 +0200

    weitere Anpassungen

--- themes/dependentworks/js/dependentworks.js
+++ themes/dependentworks/js/dependentworks.js
@@ -19,11 +19,19 @@ jQuery(document).ready(function() {
         data:{ppn:recordId},
         success:function(data, textStatus) {
             if (data.data.length > 0) {
-                jQuery('div#DependentWorks').append('<ul id="DependentWorks"></ul>');
-                for (var i = 0; i < data.data.length; i++) {
+                var visibleItems = (data.data.length < 3) ? data.data.length : 3;
+                for (var i = 0; i < visibleItems; i++) {
                     var title = data.data[i]['title'] + ' (' + data.data[i]['publishDate'] + ')';
                     var href = '<a href="/vufind/Record/' + data.data[i]['id'] + '" target="_blank">' + title + '</a>';
                     jQuery('ul#DependentWorks').append('<li>' + href + '</li>');                
+                }
+                if (data.data.length > visibleItems) {
+                    jQuery('p#ToggleDependentWorksMore').attr('style', 'display:block');
+                    for (var i = 0; i < data.data.length; i++) {
+                        var title = data.data[i]['title'] + ' (' + data.data[i]['publishDate'] + ')';
+                        var href = '<a href="/vufind/Record/' + data.data[i]['id'] + '" target="_blank">' + title + '</a>';
+                        jQuery('ul#DependentWorksHidden').append('<li>' + href + '</li>');                
+                    }
                 }
                 jQuery('div#DependentWorks').attr('style', 'display:block');
             }
