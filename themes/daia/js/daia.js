86f0cf6709fe965e4ab66dc461008efda72662dc
Kristof Kessler <kristof.kessler@tu-braunschweig.de>
2018-07-10 16:36:36 +0200

    Feature/2303

--- themes/daia/js/daia.js
+++ themes/daia/js/daia.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('.daia-availability').each(function(){
 		var $this = $(this);
         $.ajax({
-			url:'/vufind/PAIA/availability?ppn='+jQuery(this).attr('data-ppn')+'&daia='+jQuery(this).data('daia')+'&marcField951aValue='+jQuery(this).attr('data-marcField951aValue')+'&list='+jQuery(this).data('list')+'&mediatype='+jQuery(this).data('mediatype'),
+			url:'/vufind/PAIA/availability?ppn='+jQuery(this).attr('data-ppn')+'&marcField951aValue='+jQuery(this).attr('data-marcField951aValue')+'&list='+jQuery(this).data('list')+'&mediatype='+jQuery(this).data('mediatype'),
 			success:function(data) {
 				$this.html(jQuery(data).find('#paia-availability'));
 			}
