9cd7c6110eece4e005b2c325993802ec68cab604
hajoseng <hajo.seng@sub.uni-hamburg.de>
2018-07-23 15:36:25 +0200

    Korrektur

--- module/RecordDriver/config/module.config.php
+++ module/RecordDriver/config/module.config.php
@@ -5,9 +5,7 @@ $config = [
     'service_manager' => [
         'allow_override' => true,
         'factories' => [
-            'recorddriver' => [
-                'RecordDriver\RecordDriver\PluginManager' => 'RecordDriver\ServiceManager\AbstractPluginManagerFactory',
-            ],
+            'RecordDriver\RecordDriver\PluginManager' => 'RecordDriver\ServiceManager\AbstractPluginManagerFactory',
         ],
         'aliases' => [
             'RecordDriver\RecordDriverPluginManager' => 'RecordDriver\RecordDriver\PluginManager',
