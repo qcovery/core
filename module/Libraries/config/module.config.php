6eb1be08a9a5e2e8cf1f3151553500062913c863
VZG 124 <info@beluga-core.de>
2018-09-05 16:08:38 +0200

    Searchbackend repariert

--- module/Libraries/config/module.config.php
+++ module/Libraries/config/module.config.php
@@ -6,11 +6,13 @@ $config = [
         'allow_override' => true,
         'factories' => [
             'Libraries\AjaxHandler\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
+            'Libraries\Search\BackendManager' => 'Libraries\Search\BackendManagerFactory',
             'Libraries\Search\Params\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
             'Libraries\Search\Results\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
         ],
         'aliases' => [
             'VuFind\AjaxHandler\PluginManager' => 'Libraries\AjaxHandler\PluginManager',
+            'VuFind\Search\BackendManager' => 'Libraries\Search\BackendManager',
             'VuFind\Search\Params\PluginManager' => 'Libraries\Search\Params\PluginManager',
             'VuFind\Search\Results\PluginManager' => 'Libraries\Search\Results\PluginManager',
         ],
--- module/Libraries/src/Libraries/Search/BackendRegistry.php
+++ module/Libraries/src/Libraries/Search/BackendRegistry.php
@@ -26,7 +26,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-namespace VuFind\Search;
+namespace Libraries\Search;
 
 /**
  * Registry for search backends.
