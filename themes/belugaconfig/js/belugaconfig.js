cb2feddf02af6aa8a8b2901fb247b94e0b71825c
VZG 124 <info@beluga-core.de>
2018-09-10 14:31:04 +0200

    Korrekturen, Modul BelugaConfig

--- themes/belugaconfig/js/belugaconfig.js
+++ themes/belugaconfig/js/belugaconfig.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
+    function formatNumber(number) {
+        number = number.toString();
+        var len = number.length;
+        var thousands = number.substring(len-3,len);
+        var millions = number.substring(len-6,len-3);
+        var billions = number.substring(len-9,len-6);
+        var formattedNumber = '';
+        if (billions != '') {
+            formattedNumber += billions + '.';
+        }
+        if (millions != '') {
+            formattedNumber += millions + '.';
+        }
+        if (thousands != '') {
+            formattedNumber += thousands;
+        }
+        return formattedNumber;
+    }
+
     /*
      * Get result count for inactive Tabs
      */
@@ -10,8 +29,8 @@ $(document).ready(function() {
             url:'/vufind/AJAX/JSON?method=getResultCount',
             dataType:'json',
             data:{querystring:queryString, source:searchClass},
-            success:function(data, textStatus, jqXHR){
-                $this.find('a').append(' ('+data.data.total+')')
+            success:function(data, textStatus){
+                $this.find('a').append(' ('+formatNumber(data.data.total)+')')
             }
         });
     });
