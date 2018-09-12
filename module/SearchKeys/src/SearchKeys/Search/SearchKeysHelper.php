687052f235a7456cbd6d4300ccd369d68ca8f899
VZG 124 <info@beluga-core.de>
2018-09-12 15:59:33 +0200

    Anpassungen

--- module/SearchKeys/src/SearchKeys/Search/SearchKeysHelper.php
+++ module/SearchKeys/src/SearchKeys/Search/SearchKeysHelper.php
@@ -26,111 +26,94 @@ class SearchKeysHelper
     public function processSearchKeys($request, $options, $config, $searchClassId)
     {
         $id = strtolower($searchClassId);
-        $keywords = $config->get('keys-' . $id);
-        $phrasedKeywords = $config->get('phrasedKeys-' . $id);
-        $toTranslate = $config->get('translate-' . $id);
-        $isAdvancedSearch = false;
-        $lookfor = $request->get('lookfor');
-        if (!empty($lookfor)) {
-            $lookforArray = [$lookfor];
-            $typeArray = [];
-        } elseif (!empty($lookfor0)) {
-            $lookforArray = $request->get('lookfor0');
-            $typeArray = $request->get('type0');
-            $isAdvancedSearch = true;
-        } else {
-            return $request;
-        }
+        $keywords = $config->get('keys-' . $id)->toArray();
+        $phrasedKeywords = $config->get('phrasedKeys-' . $id)->toArray();
+        $toTranslate = $config->get('translate-' . $id)->toArray();
+
+        $lookfor = $lookfor = preg_replace('/\s+/', ' ', $request->get('lookfor'));
+        $lookfor = preg_replace('/""+/', '"', $lookfor);
+        $type = $request->get('type');
+        $defaultType = $options->getDefaultHandler();
 
         $searchItems = [];
-        $searchTypes = [];
         $searchBoolean = ['AND'];
         $limit = 10;
-        foreach ($lookforArray as $lookfor) {
-            $type = strval(array_shift($typeArray));
-            if (empty($lookfor) || $lookfor == '""') {
-                continue;
-            } elseif (!empty($type) && $type != $options->getDefaultHandler()) {
-                $searchItems[] = $lookfor;
-                if (isset($keywords[$type])) {
-                    $searchTypes[] = $keywords[$type];
-                } else {
-                    $searchTypes[] = $type;
-                }
-                continue;
-            }
-            $lookfor = preg_replace('/\s+/', ' ', $lookfor);
-            if (is_array($toTranslate)) {
-                foreach($toTranslate as $translateTo => $translateFrom) {
-                    $lookfor = preg_replace('/\\'.$translateFrom.'/', '{'.$translateTo.'}', $lookfor);
-                }
-            }
-            while (!empty($lookfor) && $limit-- > 0) {
-                $itemFound = false;
-                foreach ($phrasedKeywords as $keyword => $searchtype) {
-                    $searchname = $options->getHumanReadableFieldName($searchtype);
-                    $keyRegex = '(('.$keyword.'\s)|('.$searchtype.':)|('.$searchname.':))';
-                    if (preg_match('#^'.$keyRegex.'([^"]*|("[^"]*"))((?=$))#', $lookfor, $matches)) {
-                        $foundKey = $matches[1];
-                        $newLookfor = trim(str_replace($foundKey, '', $lookfor));
-                        $lookfor = '';
-                        $searchItems[] = '"'.str_replace('"', '', $newLookfor).'"';
-                        $searchTypes[] = $searchtype;
-                        $itemFound = true;
-                        $isAdvancedSearch = true;
-                        break;
-                    }
-                }
-                foreach ($keywords as $keyword => $searchtype) {
-                    $searchname = $options->getHumanReadableFieldName($searchtype);
-                    $keyRegex = '(('.$keyword.'\s)|('.$searchtype.':)|('.$searchname.':))';
-                    if (preg_match('#^'.$keyRegex.'([^"\s]*|("[^"]*"))((?=\s)|(?=$))#', $lookfor, $matches)) {
-                        $newLookfor = $matches[5];
-                        $foundKey = $matches[1];
-                        $lookfor = trim(str_replace($foundKey.$newLookfor, '', $lookfor));
-                        $searchItems[] = $newLookfor;
-                        $searchTypes[] = $searchtype;
-                        $itemFound = true;
-                        $isAdvancedSearch = true;
-                        break;
-                    }
-                }
-                if (!empty($lookfor) && !$itemFound) {
-                   if (preg_match('#^([^"\s]+|("[^"]+"))((?=\s)|(?=$))#', $lookfor, $matches)) {
-                        $newLookfor = $matches[1];
-                        $lookfor = trim(preg_replace('#^'.$newLookfor.'#', '', $lookfor));
-                        if ($newLookfor == 'OR') {
-                            $searchBoolean = [$newLookfor];
-                        } else {
-                            $searchItems[] = $newLookfor;
-                            $searchTypes[] = $options->getDefaultHandler();
-                            $itemFound = true;
-                        }
-                    }
-                }
-            }
-            if (!$itemFound) {
-                $searchItems[] = $lookfor;
-                $searchTypes[] = $type;
+
+        if (is_array($toTranslate)) {
+            foreach($toTranslate as $translateTo => $translateFrom) {
+                $lookfor = preg_replace('/\\'.$translateFrom.'/', '{'.$translateTo.'}', $lookfor);
             }
         }
 
-        if ($isAdvancedSearch) {
-            $request->set('lookfor0', null);
-            $request->set('lookfor', null);
-            if (count($searchItems) == count($searchTypes) && !empty($searchItems)) {
-                $request->set('lookfor0', $searchItems);
-                $request->set('type0', $searchTypes);
-                if (empty($request->get('bool0'))) {
-                    $request->set('bool0', $searchBoolean);
-                }
-                if (empty($request->get('op0'))) {
-                    $request->set('op0', array('AND'));
-                }
-                if (empty($request->get('join'))) {
-                    $request->set('join', 'OR');
+        $type = '';
+        while (!empty($lookfor) && $limit-- > 0) {
+            $item = $key = '';
+            foreach (array_merge($keywords, $phrasedKeywords) as $keyword => $searchType) {
+                $searchname = $options->getHumanReadableFieldName($searchtype);
+                $keyRegex = '(('.$keyword.'\s)|('.$searchType.':)|('.$searchName.':))';
+                if (preg_match('#^'.$keyRegex.'([^"\s]*|("[^"]*"))((?=\s)|(?=$))#', $lookfor, $matches)) {
+                    $key = $matches[1];
+                    $item = $matches[5];
+                    $type = $searchType;
+                    $pos = strpos($lookfor, $key);
+                    $lookfor = trim(substr_replace($lookfor, '', $pos, strlen($key)));
+                    break;
                 }
             }
+            if (empty($item)) {
+                if (preg_match('#^([^"\s]+|("[^"]+"))((?=\s)|(?=$))#', $lookfor, $matches)) {
+                    $item = $matches[1];
+                    $type = (empty($type)) ? $defaultType : $type;
+                    $pos = strpos($lookfor, $item);
+                    $lookfor = trim(substr_replace($lookfor, '', $pos, strlen($item)));
+                    if ($item == 'OR') {
+                        $searchBoolean = ['OR'];
+                        $item = '';
+                    }
+                }
+                if (!empty($item)) {
+                    if (empty($type)) {
+                        $type = $defaultType;
+                    }
+                    if (!isset($searchItems[$type])) {
+                        $searchItems[$type] = array();
+                    }
+                    $searchItems[$type][] = $item;
+                }
+            }
+        }
+
+        $lookfors = $types = array();
+        foreach ($searchItems as $type => $items) {
+            $types[] = $type;
+            $lookfor = implode(' ', $items);
+            if (in_array($type, array_keys($phrasedKeywords))) {
+                $lookfor = '"' . $lookfor . '"';
+            }
+            $lookfors[] = $lookfor;
+        }
+
+        $request->set('lookfor0', null);
+        $request->set('lookfor', null);
+        if (count($lookfors) > 1) {
+            $request->set('lookfor0', $lookfors);
+            $request->set('type0', $types);
+            if (empty($request->get('bool0'))) {
+                $request->set('bool0', $searchBoolean);
+            }
+            if (empty($request->get('op0'))) {
+                $request->set('op0', array('AND'));
+            }
+            if (empty($request->get('join'))) {
+                $request->set('join', 'OR');
+            }
+        } elseif (count($lookfors) == 1) {
+            $request->set('lookfor', $lookfors[0]);
+            if ($types[0] != $defaultType) {
+                $request->set('type', $types[0]);
+            }
+        } else {
+            $request->set('lookfor', '');
         }
         return $request;
     }
--- themes/searchkeys/mixin.config.php
+++ themes/searchkeys/mixin.config.php
@@ -3,13 +3,9 @@ return [
     'helpers' => [
         'factories' => [
             'SearchKeys\View\Helper\SearchKeys\SearchBox' => 'SearchKeys\View\Helper\SearchKeys\SearchBoxFactory',
-//            'SearchKeys\View\Helper\SearchKeys\SearchParams' => 'SearchKeys\View\Helper\SearchKeys\SearchParamsFactory',
-            'SearchKeys\View\Helper\SearchKeys\SearchTabs' => 'SearchKeys\View\Helper\SearchKeys\SearchTabsFactory',
         ],
         'aliases' => [
             'searchbox' => 'SearchKeys\View\Helper\SearchKeys\SearchBox',
-//            'searchParams' => 'SearchKeys\View\Helper\SearchKeys\SearchParams',
-            'searchTabs' => 'SearchKeys\View\Helper\SearchKeys\SearchTabs',
         ],
     ],
 ];
