eb5716a4c489152f93676d769e6e42daf845daf3
Johannes Schultze <schultze@effective-webwork.de>
2018-11-28 10:18:27 +0100

    Converting marc21 to turbomarc.

--- module/IMS/src/IMS/Controller/CartController.php
+++ module/IMS/src/IMS/Controller/CartController.php
@@ -48,27 +48,73 @@ class CartController extends \VuFind\Controller\CartController
 
         $records = $this->getRecordLoader()->loadBatch($ids);
 
-        $results = [];
-        foreach ($records as $record) {
-            $temp = tmpfile();
-            fwrite($temp, $record->getXML('marc21'));
-            fseek($temp, 0);
-
-            $command = 'yaz-marcdump -i marcxml -o turbomarc '.stream_get_meta_data($temp)['uri'];
-            exec($command, $execResults);
-
-            fclose($temp); // dies entfernt die Datei
-
-            $results[] = $execResults;
+        $imsBaseDir = getcwd().'/var/ims/';
+        if (!is_dir($imsBaseDir)) {
+            mkdir($imsBaseDir);
         }
+        if ($fileDownload = fopen($imsBaseDir.$imsid.'-turbomarc.xml', 'w')) {
+            $turbomarcData = '';
+            foreach ($records as $record) {
+                $temp = tmpfile();
+                fwrite($temp, $record->getXML('marc21'));
+                fseek($temp, 0);
 
-        //error_log(print_r($records, true));
+                $command = 'yaz-marcdump -i marcxml -o turbomarc '.stream_get_meta_data($temp)['uri'];
+                exec($command, $execResults);
+
+                fclose($temp); // dies entfernt die Datei
+
+                $turbomarcData .= implode("\n", $execResults);
+            }
+            fwrite($fileDownload, $turbomarcData);
+            fclose($fileDownload);
+        }
 
         // Send appropriate HTTP headers for requested format:
         $response = $this->getResponse();
 
         // Process and display the exported records
-        $response->setContent(json_encode(['imsDownloadUrl' => urlencode('http://localhost:8080/vufind/Cart/imsdownload?id='.$imsid), 'results' => $results]));
+        $response->setContent(json_encode(['imsDownloadUrl' => urlencode('http://localhost:8080/vufind/Cart/imsdownload?imsid='.$imsid)]));
+        return $response;
+    }
+
+    /**
+     * IMS action to export file.
+     *
+     * @return mixed
+     */
+    public function imsdownloadAction()
+    {
+        // Bail out if cart is disabled.
+        if (!$this->getCart()->isActive()) {
+            return $this->redirect()->toRoute('home');
+        }
+
+        // If a user is coming directly to the cart, we should clear out any
+        // existing context information to prevent weird, unexpected workflows
+        // caused by unusual user behavior.
+        $this->followup()->retrieveAndClear('cartAction');
+        $this->followup()->retrieveAndClear('cartIds');
+
+        // We use abbreviated parameters here to keep the URL short (there may
+        // be a long list of IDs, and we don't want to run out of room):
+        $imsid = $this->params()->fromQuery('imsid');
+
+        $response = $this->getResponse();
+
+        $result = '';
+
+        $imsBaseDir = getcwd().'/var/ims/';
+        $imsFile = $imsBaseDir . $imsid . '-turbomarc.xml';
+        if (file_exists($imsFile)) {
+            if ($fileDownload = fopen($imsFile, 'r')) {
+                $result = fread($fileDownload, filesize($imsFile));
+                fclose($fileDownload);
+            }
+        }
+
+        // Process and display the exported records
+        $response->setContent($result);
         return $response;
     }
 }
