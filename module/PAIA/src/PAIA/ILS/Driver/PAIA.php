41b19aa033710157ed1b4fc3d375b31e6bbe9803
Johannes Schultze <schultze@effective-webwork.de>
2019-02-20 10:50:08 +0100

    Format dates.

--- module/PAIA/src/PAIA/ILS/Driver/PAIA.php
+++ module/PAIA/src/PAIA/ILS/Driver/PAIA.php
@@ -393,7 +393,7 @@ class PAIA extends AbstractBase
             'firstname' => $name_array[1],
             'lastname' => $name_array[0],
             'email' => $json_array['email'],
-            'expires' => $json_array['expires'],
+            'expires' => $expiresDate->format('d.m.Y'),
             'status' => $json_array['status'],
             'address' => $json_array['address'],
             'type' => $json_array['type'][0],
