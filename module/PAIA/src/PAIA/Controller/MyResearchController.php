2f776956f17d0f38c3444bb066b67e2681477c47
Johannes Schultze <schultze@effective-webwork.de>
2019-02-27 10:55:05 +0100

    #2855 - Icons: Texte und Austausch.

--- module/PAIA/src/PAIA/Controller/MyResearchController.php
+++ module/PAIA/src/PAIA/Controller/MyResearchController.php
@@ -424,7 +424,8 @@ class MyResearchController extends AbstractBase
             $this->favorites()->delete($ids, $listID, $user);
             $this->flashMessenger()->setNamespace('info')
                 ->addMessage('fav_delete_success');
-            return $this->redirect()->toUrl($newUrl);
+            //return $this->redirect()->toUrl($newUrl);
+            return $this->forwardTo('MyResearch', 'deleteConfirm');
         }
 
         // If we got this far, the operation has not been confirmed yet; show
@@ -441,6 +442,9 @@ class MyResearchController extends AbstractBase
                 'records' => $this->getRecordLoader()->loadBatch($ids)
             )
         );
+    }
+
+    public function deleteConfirmAction () {
     }
 
     /**
