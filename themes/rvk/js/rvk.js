efa156e9a30709c2df0548ed4f3f884bdc5d4a07
Johannes Schultze <schultze@effective-webwork.de>
2019-03-05 16:09:32 +0100

    Added option to display RVK details as overlay.

--- themes/rvk/js/rvk.js
+++ themes/rvk/js/rvk.js
@@ -1,16 +1,40 @@
 $(document).ready(function() {
     jQuery('.rvk-wrapper').each(function(event){
         var rvk = $(this);
-        rvk.find('.rvk-status').addClass('spinner');
-        rvk.find('.rvk-status').addClass('bel-laden01');
+        var rvkStatus = rvk.find('.rvk-status');
+        var rvkDetails = rvk.find('.rvk-details');
+        var rvkLink = rvk.find('.rvk-link');
+
+        rvkStatus.addClass('spinner');
+        rvkStatus.addClass('bel-laden01');
         jQuery.ajax({
             url:'/vufind/AJAX/JSON?method=getRVKStatus&rvk='+rvk.find('.rvk-details').attr('data-rvk'),
             dataType:'json',
             success:function(data, textStatus, jqXHR){
-                rvk.find('.rvk-status').removeClass('spinner');
-                rvk.find('.rvk-status').removeClass('bel-laden01');
-                rvk.find('.rvk-details').addClass('rvk-details-show');
-                rvk.find('.rvk-details').html(data.data.join('<i class="bel-pfeil-l01"></i>'));
+                rvkStatus.removeClass('spinner');
+                rvkStatus.removeClass('bel-laden01');
+                if (typeof rvkDetails.data('rvk-hover') === 'undefined') {
+                    rvkDetails.addClass('rvk-details-show');
+                    rvkDetails.html(data.data.join('<i class="bel-pfeil-l01"></i>'));
+                } else {
+                    rvkDetails.addClass('rvk-details-hide');
+                    rvkDetails.html(data.data.join('<i class="bel-pfeil-l01"></i>'));
+                    //rvkLink.attr('data-uk-tooltip', 'true');
+                    //rvkLink.attr('title', data.data.join('<i class="bel-pfeil-l01"></i>'));
+
+                    rvkLink.qtip({
+                        content: {
+                            text: data.data.join('<i class="bel-pfeil-l01"></i>'),
+                        },
+                        position: {
+                            my: 'middle left',
+                            at: 'middle right',
+                        },
+                        style: {
+                            classes: 'qtip-light'
+                        }
+                    });
+                }
             }
         });
     });
