93d40b3a28919a80de20c837ac7253ae8ad0df03
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2019-03-13 12:07:54 +0100

    Modul Delivery Authentifizierung, Anzeige

--- themes/delivery/mixin.config.php
+++ themes/delivery/mixin.config.php
@@ -3,9 +3,11 @@ return [
     'helpers' => [
         'factories' => [
             'Delivery\View\Helper\Delivery\AvailabilityChecker' => 'Delivery\View\Helper\Delivery\AvailabilityCheckerFactory',
+            'Delivery\View\Helper\Delivery\Authenticator' => 'Delivery\View\Helper\Delivery\AuthenticatorFactory',
         ],
         'aliases' => [
             'availabilitychecker' => 'Delivery\View\Helper\Delivery\AvailabilityChecker',
+            'authenticator' => 'Delivery\View\Helper\Delivery\Authenticator',
         ]
     ]
 ];
