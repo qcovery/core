d41d72be2984b66368ee957564b4ec4d4411e6c7
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2019-04-29 16:47:13 +0200

    Multilogin

--- module/PAIAplus/config/module.config.php
+++ module/PAIAplus/config/module.config.php
@@ -5,10 +5,13 @@ $config = [
     'service_manager' => [
         'allow_override' => true,
         'factories' => [
+            'PAIAplus\Auth\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticatorFactory',
             'PAIAplus\ILS\Driver\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
             'PAIAplus\ILS\Connection' => 'PAIAplus\ILS\ConnectionFactory',
         ],
         'aliases' => [
+            'VuFind\Auth\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticator',
+            'VuFind\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticator',
             'VuFind\ILS\Driver\PluginManager' => 'PAIAplus\ILS\Driver\PluginManager',
             'VuFind\ILSDriverPluginManager' => 'PAIAplus\ILS\Driver\PluginManager',
             'VuFind\ILS\Connection' => 'PAIAplus\ILS\Connection',
