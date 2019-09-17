81eccda32ece7254173d954e8e9056e930f63082
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2019-09-17 15:46:26 +0200

    Vorabprüfung auf mögliche Volltexte vor Abfrage von DAIAplus (konfigurierbar)

--- module/DAIAplus/src/DAIAplus/AjaxHandler/GetArticleStatuses.php
+++ module/DAIAplus/src/DAIAplus/AjaxHandler/GetArticleStatuses.php
@@ -50,6 +50,7 @@ use Zend\Mvc\Controller\Plugin\Params;
  */
 class GetArticleStatuses extends AbstractBase
 {
+    use \VuFind\I18n\Translator\TranslatorAwareTrait;
 
     protected $recordLoader;
     
@@ -81,62 +82,58 @@ class GetArticleStatuses extends AbstractBase
         $responses = [];
         $ids = $params->fromPost('id', $params->fromQuery('id', ''));
         $source = $params->fromPost('source', $params->fromQuery('source', ''));
+        $resolverChecks = $this->config['ResolverChecks'];
         if (!empty($ids) && !empty($source)) {
             $listView = $params->fromPost('list', $params->fromQuery('list', '0'));
-
             foreach ($ids as $id) {
                 $driver = $this->recordLoader->load($id, $source);
 
-/*
-hier erst prüfen: $driver->getMarcData('ContainingWorkPpn'): link. Oder ContainingWorkXyz oder extra Kategorie?
-*/
-
-                $openUrl = $driver->getOpenUrl();
-                $formats = $driver->getFormats();
-                $format = $formats[0];
-                $format = strtolower(str_ireplace('electronic ','',$format));
-
-                $sfxLink = $openUrl;
-                $sfxData = $driver->getMarcData('SFX');
-
-                if (is_array($sfxData)) {
-                    foreach ($sfxData as $sfxDate) {
-                        if (is_array($sfxDate)) {
-                            foreach ($sfxDate as $key => $value) {
-                                $sfxLink .= '&' . $key . '=' . urlencode($value['data'][0]);
-                                if(strpos($openUrl, 'rft.' . $key . '=') === false) {
-                                    $openUrl .= '&rft.' . $key . '=' . urlencode($value['data'][0]);
-                                }
-                            }
-                        }
+                $urlAccess = '';
+                if (isset($resolverChecks['fulltext']) && $resolverChecks['fulltext'] == 'y') {
+                    $urlAccess = $this->checkFulltext($driver);
+                    if (!empty($urlAccess)) {
+                        $urlAccessLevel = 'article_access_level';
+                        $urlAccessLabel = 'Fulltext';
+                    }
+                }
+                if (empty($urlAccess) && isset($resolverChecks['open_access']) && $resolverChecks['open_access'] == 'y') {
+                    $urlAccess = $this->checkOpenAccess($driver);
+                    if (!empty($urlAccess)) {
+                        $urlAccessLevel = 'article_access_level';
+                        $urlAccessLabel = 'Fulltext (DOAJ)';
+                    }
+                }
+                if (empty($urlAccess) && isset($resolverChecks['doi']) && $resolverChecks['doi'] == 'y') {
+                    $urlAccess = $this->checkDoi($driver);
+                    if (!empty($urlAccess)) {
+                        $urlAccessLevel = 'article_access_level';
+                        $urlAccessLabel = 'Fulltext (DOI)';
+                    }
+                }
+                if (empty($urlAccess) && isset($resolverChecks['journal']) && $resolverChecks['journal'] == 'y') {
+                    $urlAccess = $this->checkParentId($driver);
+                    if (!empty($urlAccess)) {
+                        $urlAccessLevel = 'print_access_level';
+                        $urlAccessLabel = 'Journal';
                     }
                 }
 
-                if(strpos($openUrl, 'rft.genre=') === false) {
-                    $openUrl .= '&rft.genre=' . $format;
+                if (!empty($urlAccess)) {
+                    $response = ['list' => ['url_access' => $urlAccess,
+                                            'url_access_level' => $urlAccessLevel,
+                                            'url_access_label' => $urlAccessLabel,
+                                            'link_status' => 1],
+                                'items' => ['lr_check' =>
+                                            ['url_access' => $urlAccess,
+                                            'url_access_level' => $urlAccessLevel,
+                                            'url_access_label' => $urlAccessLabel,
+                                            'link_status' => 1]
+                                ]
+                    ];
+                } else {
+                    $url = $this->prepareUrl($driver, $id, $listView);
+                    $response = json_decode($this->makeRequest($url), true);
                 }
-
-                $sfxDomain = $this->config['DAIA']['sfxDomain'] ?? '';
-                $sfxLink = urlencode('http://sfx.gbv.de/sfx_' . $sfxDomain . '?' . $sfxLink);
-
-                $isil = $this->config['Global']['isil'];
-                $url = $this->config['DAIA_' . $isil]['url'];
-                $url .= 'electronicavailability/' . $id . '?';
-                $url .= 'apikey=' . $this->config['DAIA_' . $isil]['daiaplus_api_key'];
-                $url .= '&openurl=' . urlencode($openUrl);
-                $url .= '&list=' . $listView;
-                $url .= '&mediatype=' . urlencode($format);
-                if ($sfxDomain) {
-                    $url .= '&sfx=' . $sfxLink;
-                }
-                $url .= '&language=de';
-                $url .= '&format=json';
-
-                $response = json_decode($this->makeRequest($url), true);
-/*
-Falls ContainingWork: Daten für prepareData vorbereiten
-
-*/
 
                 $response = $this->prepareData($response, $listView);
                 $response['id'] = $id;
@@ -144,6 +141,104 @@ Falls ContainingWork: Daten für prepareData vorbereiten
             }
         }
         return $this->formatResponse(['statuses' => $responses]);
+    }
+
+    private function checkDoi($driver) {
+        $urlAccess = '';
+        $doiData = $driver->getMarcData('ArticleDoi');
+        foreach ($doiData as $doiDate) {
+            if (!empty(($doiDate['doi']['data'][0]))) {
+                $urlAccess = 'http://dx.doi.org/' . $doiDate['doi']['data'][0];
+                break;
+            }
+        }
+        return $urlAccess;
+    }                
+
+    private function checkOpenAccess($driver) {
+        $urlAccess = '';
+        $doajData = $driver->getMarcData('ArticleDoaj');
+        foreach ($doajData as $doajDate) {
+            if (!empty(($doajDate['url']['data'][0]))) {
+                $urlAccess = $doajDate['url']['data'][0];
+                break;
+            }
+        }
+        return $urlAccess;
+    }                
+
+    private function checkFulltext($driver) {
+        $urlAccess = '';
+        $fulltextData = $driver->getMarcData('ArticleFulltext');
+        foreach ($fulltextData as $fulltextDate) {
+            if (!empty(($fulltextDate['url']['data'][0]))) {
+                $urlAccess = $fulltextDate['url']['data'][0];
+                break;
+            }
+        }
+        return $urlAccess;
+    }
+
+    private function checkParentId($driver) {
+        $urlAccess = '';
+        $parentData = $driver->getMarcData('ArticleParentId');
+        foreach ($parentData as $parentDate) {
+            if (!empty(($parentDate['id']['data'][0]))) {
+                $parentId = $parentDate['id']['data'][0];
+                break;
+            }
+        }
+        if (!empty($parentId)) {
+            $parentDriver = $this->recordLoader->load($parentId, 'Solr');
+            $ilnMatch = $parentDriver->getMarcData('ILN');
+            if (!empty($ilnMatch[0]['iln']['data'][0])) {
+                $urlAccess = '/vufind/Record/' . $parentId;
+            }
+        }
+        return $urlAccess;
+    }
+
+    private function prepareUrl($driver, $id, $listView) {
+        $openUrl = $driver->getOpenUrl();
+        $formats = $driver->getFormats();
+        $format = strtolower(str_ireplace('electronic ','',$formats[0]));
+
+        $sfxLink = $openUrl;
+        $sfxData = $driver->getMarcData('SFX');
+
+        if (is_array($sfxData)) {
+            foreach ($sfxData as $sfxDate) {
+                if (is_array($sfxDate)) {
+                    foreach ($sfxDate as $key => $value) {
+                        $sfxLink .= '&' . $key . '=' . urlencode($value['data'][0]);
+                        if(strpos($openUrl, 'rft.' . $key . '=') === false) {
+                            $openUrl .= '&rft.' . $key . '=' . urlencode($value['data'][0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(strpos($openUrl, 'rft.genre=') === false) {
+            $openUrl .= '&rft.genre=' . $format;
+        }
+
+        $sfxDomain = $this->config['DAIA']['sfxDomain'] ?? '';
+        $sfxLink = urlencode('http://sfx.gbv.de/sfx_' . $sfxDomain . '?' . $sfxLink);
+
+        $isil = $this->config['Global']['isil'];
+        $url = $this->config['DAIA_' . $isil]['url'];
+        $url .= 'electronicavailability/' . $id . '?';
+        $url .= 'apikey=' . $this->config['DAIA_' . $isil]['daiaplus_api_key'];
+        $url .= '&openurl=' . urlencode($openUrl);
+        $url .= '&list=' . $listView;
+        $url .= '&mediatype=' . urlencode($format);
+        if ($sfxDomain) {
+            $url .= '&sfx=' . $sfxLink;
+        }
+        $url .= '&language=de';
+        $url .= '&format=json';
+        return $url;
     }
 
     private function makeRequest($url) {
@@ -159,35 +254,43 @@ Falls ContainingWork: Daten für prepareData vorbereiten
     }
 
     private function prepareData($rawData, $list) {
+        $resolverLabels = $this->config['ResolverLabels'];
         $data = [];
         if (!empty($rawData['items']['sfx'])) {
+            $label = $resolverLabels['article'] ?: 'SFX';
             $data[] = [
                 'href' => $rawData['items']['sfx'],
                 'level' => 'article_access_level',
-                'label' => 'SFX'
+                'label' => $this->translate($label)
             ];
         } elseif (!empty($rawData)) {
             if ($list == 1 && !empty($rawData['list']['url_access'])) {
                 $urlAccess = (is_array($rawData['list']['url_access'])) ? $rawData['list']['url_access'][0] : $rawData['list']['url_access'];
+                $level = str_replace('_access_level', '', $rawData['list']['url_access_level']);
+                $label = $resolverLabels[$level] ?: $rawData['list']['url_access_label'];
                 $data[] = [
                     'href' => $urlAccess,
                     'level' => $rawData['list']['url_access_level'],
-                    'label' => $rawData['list']['url_access_label'],
+                    'label' => $this->translate($label),
                     'doi' => $rawData['list']['doi']
                 ];
             } else {
                 if (empty($rawData['list']['url_access'])) {
+                    $level = str_replace('_access_level', '', $rawData['list']['url_access_level']);
+                    $label = $resolverLabels[$level] ?: $rawData['list']['url_access_label'];
                     $data[] = [
-                        'label' => $rawData['list']['url_access_label']
+                        'label' => $this->translate($label)
                     ];
                 } else {
                     foreach ($rawData['items'] as $item) {
                         if (!empty($item) && !empty($item['url_access'])) {
                             $urlAccess = (is_array($item['url_access'])) ? $item['url_access'][0] : $item['url_access'];
+                            $level = str_replace('_access_level', '', $item['url_access_level']);
+                            $label = $resolverLabels[$level] ?: $item['url_access_label'];
                             $data[] = [
                                 'href' => $urlAccess,
                                 'level' => $item['url_access_level'],
-                                'label' => $item['url_access_label'],
+                                'label' => $this->translate($label, [], $label),
                                 'notification' => $item['access_notification'],
                                 'doi' => $item['doi']
                             ];
