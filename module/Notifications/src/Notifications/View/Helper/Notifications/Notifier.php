32865f5af00e4acbad8d32171b86981d8b5d11ce
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2020-03-26 09:45:13 +0100

    Korrektur in der Auswertung der Konfiguration

--- module/Notifications/src/Notifications/View/Helper/Notifications/Notifier.php
+++ module/Notifications/src/Notifications/View/Helper/Notifications/Notifier.php
@@ -28,42 +28,51 @@ class Notifier extends AbstractHelper
      */
     public function getNotifications($path) {
         $path = rtrim($path, '/');
+        $actualIp = $_SERVER['REMOTE_ADDR'];
         $messages = [];
         foreach ($this->notificationsConfig as $event => $action) {
+            $ipOk = $pathOk = false;
             foreach ($action['conditions'] as $condition) {
                 list($type, $criterion) = explode(':', $condition);
-                if ($type == 'iprange') {
-                    $actualIp = $_SERVER['REMOTE_ADDR'];
+                if ($type == 'ip') {
                     if (strpos($criterion, '!') === 0) {
                         $criterion = substr($criterion, 1);
-                        if (strpos($actualIp, $criterion) === 0) {
+                        if (strpos($actualIp, $criterion) !== 0) {
+                            $ipOk = true;
+                        } else {
                             continue 2;
                         }
-                    } elseif (strpos($actualIp, $criterion) !== 0) {
-                        continue 2;
+                    } elseif (strpos($actualIp, $criterion) === 0) {
+                        $ipOk = true;
                     }
                 } elseif ($type == 'path') {
                     if (strpos($criterion, '!') === 0) {
                         $criterion = substr($criterion, 1);
                         if ($criterion == 'home') {
-                            if ($path == '/vufind' || $path == $this->defaultHome) {
-                                 continue 2;
-                            }
-                        } elseif ($path == $criterion) {
-                             continue 2;
-                        }
-                    } else {
-                        if ($criterion == 'home') {
                             if ($path != '/vufind' && $path != $this->defaultHome) {
+                                 $pathOk = true;
+                            } else {
                                  continue 2;
                             }
                         } elseif ($path != $criterion) {
-                             continue 2;
+                             $pathOk = true;
+                        } else {
+                            continue 2;
+                        }
+                    } else {
+                        if ($criterion == 'home') {
+                            if ($path == '/vufind' || $path == $this->defaultHome) {
+                                $pathOk = true;
+                            }
+                        } elseif ($path == $criterion) {
+                             $pathOk = true;
                         }
                     }
                 }
             }
-            $messages[] = $action['message'];
+            if ($ipOk && $pathOk) {
+                $messages[] = $action['message'];
+            }
         }
         return $messages;
     }
