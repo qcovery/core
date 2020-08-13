0ee350819a64ae6547af5e5baef7abde16792708
Kristof Keßler <kristof.kessler@tu-braunschweig.de>
2020-08-13 10:32:47 +0200

    Generalisierung Funktion checkFreeAccess
    Damit muss die Funktion nun in der Regel nicht mehr geändert werden, wenn es neue SolrMarcKeys in der Konfigurationsdatei freeaccesscheck.yaml gibt.

--- module/DAIAplus/src/DAIAplus/AjaxHandler/GetArticleStatuses.php
+++ module/DAIAplus/src/DAIAplus/AjaxHandler/GetArticleStatuses.php
@@ -131,41 +131,30 @@ class GetArticleStatuses extends AbstractBase implements TranslatorAwareInterfac
     }
 
     private function checkFreeAccess($driver, $urlAccessUncertain = '') {
-        $urlAccess = '';
-        $data = $driver->getMarcData('ArticleFulltextFree');
-        foreach ($data as $date) {
-            if (!empty(($date['url']['data'][0]))) {
-                $urlAccess = $date['url']['data'][0];
-                break;
-            }
-        }
+        $urlAccess = '';      
+        $categories = array("marcFulltextCheckDirect", "marcFulltextCheckIndirect");
         
-        $data = $driver->getMarcData('ArticleFulltextFreeUrlParts');
-        foreach ($data as $date) {
-            if (!empty(($date['url']['data'][0]))) {
-                $urlAccess = $date['url']['data'][0];
-                break;
-            }
-        }
-        
-        $data = $driver->getMarcData('ArticleFulltextFreeUrlParts2');
-        foreach ($data as $date) {
-            if (!empty(($date['url']['data'][0]))) {
-                $urlAccess = $date['url']['data'][0];
-                break;
-            }
-        }
-
-        if($urlAccessUncertain) {
-                    $data = $driver->getMarcData('ArticleFulltextFreeCollection');
-            foreach ($data as $date) {
-                if (!empty(($date['collection']['data'][0]))) {
-                    $urlAccess = $urlAccessUncertain;
-                    break;
+        foreach ($categories as $category) {
+            error_log($category);
+            foreach ($driver->getSolrMarcKeys($category) as $solrMarcKey) {
+                error_log($solrMarcKey);
+                $data = $driver->getMarcData($solrMarcKey);
+                foreach ($data as $date) {
+                    if ($category == "marcFulltextCheckDirect") {
+                        if (!empty(($date['url']['data'][0]))) {
+                            $urlAccess = $date['url']['data'][0];
+                            break;
+                        }
+                    } else if ($urlAccessUncertain && $category == "marcFulltextCheckIndirect") {
+                        if (!empty(($date))) {
+                            $urlAccess = $urlAccessUncertain;
+                            break;
+                        }
+                    }
                 }
             }
         }
-        
+       
         return $urlAccess;
     }                
 
