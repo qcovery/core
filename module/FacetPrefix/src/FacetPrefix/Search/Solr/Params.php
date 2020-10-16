98095efea931cf64e79fb0caed0060728ef4762b
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2020-10-16 11:50:23 +0200

    Anpassung an die Änderungen, die sich durch den Pullrequest bei vufind-org ergeben haben. Vorsicht: Die Paramater stehen jetzt in der facets.ini->Results_Settings und heißen facet_prefix_by_field bzw. facet_matches_by_field. Beispiele und Beschreibung finden sich in config/vufind/facets.ini

--- module/FacetPrefix/src/FacetPrefix/Search/Solr/Params.php
+++ module/FacetPrefix/src/FacetPrefix/Search/Solr/Params.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Params Extension for FacetPrefix Module
+ * Solr aspect of the Search Multi-class (Params)
  *
- * PHP version 5
+ * PHP version 7
  *
- * Copyright (C) Staats- und Universitätsbibliothek 2017.
+ * Copyright (C) Villanova University 2011.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -17,39 +17,48 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * @category VuFind2
- * @package  Search
- * @author   Hajo Seng <hajo.seng@sub.uni-hamburg.de>
+ * @category VuFind
+ * @package  Search_Solr
+ * @author   Demian Katz <demian.katz@villanova.edu>
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
- * @link     https://github.com/subhh/beluga
+ * @link     https://vufind.org Main Page
  */
 namespace FacetPrefix\Search\Solr;
 
-use Libraries\Libraries;
 use VuFindSearch\ParamBag;
 use VuFind\Search\Solr\HierarchicalFacetHelper;
-use SearchKeys\Search\SearchKeysHelper;
+use VuFind\Search\Solr\Params as BaseParams;
 
-//use VuFind\Search\Solr\Params as BaseParams;
-use Libraries\Search\Solr\Params as BaseParams;
-
+/**
+ * Solr Search Parameters
+ *
+ * @category VuFind
+ * @package  Search_Solr
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Page
+ */
 class Params extends BaseParams
 {
+    use \VuFind\Search\Params\FacetLimitTrait;
+    use \FacetPrefix\Search\Params\FacetRestrictionsTrait;
 
     /**
      * Constructor
      *
      * @param \VuFind\Search\Base\Options  $options      Options to use
      * @param \VuFind\Config\PluginManager $configLoader Config loader
+     * @param HierarchicalFacetHelper      $facetHelper  Hierarchical facet helper
      */
     public function __construct($options, \VuFind\Config\PluginManager $configLoader,
-        HierarchicalFacetHelper $facetHelper = null,
-        SearchKeysHelper $searchKeysHelper,
-        \VuFind\Search\Memory $searchMemory
+        HierarchicalFacetHelper $facetHelper = null
     ) {
-        parent::__construct($options, $configLoader, $facetHelper, $searchKeysHelper, $searchMemory);
+        parent::__construct($options, $configLoader);
+
+        $config = $configLoader->get($options->getFacetsIni());
+        $this->initFacetRestrictionsFromConfig($config->Results_Settings ?? null);
     }
 
     /**
@@ -59,22 +68,22 @@ class Params extends BaseParams
      */
     public function getFacetSettings()
     {
-        $facetSet = parent::getFacetSettings();
+        // Build a list of facets we want from the index
+        $facetSet = [];
 
-        $facetConfig = $this->configLoader->get('facets');
-        if (isset($facetConfig->FacetPrefix)) {
-            foreach ($facetConfig->FacetPrefix as $facet => $prefix) {
-                $facetSet["f.{$facet}.facet.prefix"] = $prefix;
+        if (!empty($this->facetConfig)) {
+            $facetSet = parent::getFacetSettings();
+            foreach (array_keys($this->facetConfig) as $facetField) {
+                $fieldPrefix = $this->getFacetPrefixForField($facetField);
+                if (!empty($fieldPrefix)) {
+                    $facetSet["f.{$facetField}.facet.prefix"] = $fieldPrefix;
+                }
+                $fieldMatches = $this->getFacetMatchesForField($facetField);
+                if (!empty($fieldMatches)) {
+                    $facetSet["f.{$facetField}.facet.matches"] = $fieldMatches;
+                }
             }
         }
-		
-		if (isset($facetConfig->FacetMatches)) {
-            foreach ($facetConfig->FacetMatches as $facet => $matches) {
-                $facetSet["f.{$facet}.facet.matches"] = $matches;
-            }
-        }
-
         return $facetSet;
     }
 }
-
