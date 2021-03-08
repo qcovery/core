37cbfbde6d81937225493d68ea37fe327c1dbc9a
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2021-03-08 13:07:00 +0100

    Theme.config

---
+++ themes/bootstrap3plus/theme.config.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'extends' => 'bootstrap3',
+    'css' => [
+        'jquery.qtip.min.css',
+        'uikit.min.css',
+        'compiled.css',
+        'belugino.css',
+        'belugax.css',
+    ],
+    'js' => [
+        'jquery.qtip.min.js',
+        'uikit.min.js',
+        'belugax.js',
+    ],
+    'mixins' => [
+MIXINS
+    ],
+    "less" => [
+        "active" => false,
+        "compiled.less"
+    ],
+];
