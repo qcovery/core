5f1a8fee1e2fe2a46a7d5e257b136fe426e73ee6
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2021-03-11 08:32:47 +0100

    Anpassungen

--- themes/bootstrap3plus/theme.config.php
+++ themes/bootstrap3plus/theme.config.php
@@ -2,24 +2,8 @@
 
 return [
     'extends' => 'bootstrap3',
-    'css' => [
-        'jquery.qtip.min.css',
-        'uikit.min.css',
-        'compiled.css',
-        'belugino.css',
-        'belugax.css',
-    ],
-    'js' => [
-        'jquery.qtip.min.js',
-        'uikit.min.js',
-        'belugax.js',
-    ],
     'mixins' => [
 MIXINS
         'beluga-core-base',
-    ],
-    "less" => [
-        "active" => false,
-        "compiled.less"
     ],
 ];
