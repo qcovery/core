c9434e7ef2e048ec109fc9f77fa1b88306829366
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2021-09-23 10:48:07 +0200

    Facethandling ins Facettenmodul

---
+++ themes/extendedfacets/js/extendedfacets.js
@@ -0,0 +1,11 @@
+function moreFacets(id) {
+  $('.' + id).removeClass('hidden');
+  $('#more-' + id).addClass('hidden');
+  return false;
+}
+function lessFacets(id) {
+  $('.' + id).addClass('hidden');
+  $('#more-' + id).removeClass('hidden');
+  return false;
+}
+
--- themes/extendedfacets/mixin.config.php
+++ themes/extendedfacets/mixin.config.php
@@ -1,5 +1,7 @@
 <?php
 return [
+return [
+    'js' => 'extendedfacets.js',
     'helpers' => [
         'factories' => [
             'ExtendedFacets\View\Helper\ExtendedFacets\ExtendedFacets' => 'ExtendedFacets\View\Helper\ExtendedFacets\ExtendedFacetsFactory',
