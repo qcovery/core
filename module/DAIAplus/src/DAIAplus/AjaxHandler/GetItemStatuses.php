11bc5f0b9f21fee5ca9a8780bc64b18192d58944
VZG 124 <info@beluga-core.de>
2021-11-15 12:47:18 +0100

    Bugfix

--- module/DAIAplus/src/DAIAplus/AjaxHandler/GetItemStatuses.php
+++ module/DAIAplus/src/DAIAplus/AjaxHandler/GetItemStatuses.php
@@ -109,7 +109,7 @@ class GetItemStatuses extends \VuFind\AjaxHandler\GetItemStatuses implements Tra
 				$responses = [];
 				$response = [];
                 foreach($this->checks as $check => $this->current_mode) {
-                    if(in_array($check_mode,array('continue','always'))) {
+                    if(in_array($check_mode,array('continue')) || in_array($this->current_mode,array('always'))) {
                         $results = $this->performAvailabilityCheck($check);
                         foreach($results as $result) {
 							if(!empty($result)) {
