6be131cfdc2d595e987101d6a89bb630a557dba1
kkessler <mail@kristofkessler.com>
2022-01-04 14:34:29 +0100

    updated availabilityplus theme

--- themes/availabilityplus/mixin.config.php
+++ themes/availabilityplus/mixin.config.php
@@ -4,12 +4,4 @@ return [
     'js' => [
         'check_item_statuses.js',
     ],
-    'helpers' => [
-        'factories' => [
-            'DAIAplus\View\Helper\DAIAplus\DAIAplusProcessor' => 'DAIAplus\View\Helper\DAIAplus\DAIAplusProcessorFactory',
-        ],
-        'aliases' => [
-            'DAIAplusProcessor' => 'DAIAplus\View\Helper\DAIAplus\DAIAplusProcessor',
-        ]
-    ]
 ];
