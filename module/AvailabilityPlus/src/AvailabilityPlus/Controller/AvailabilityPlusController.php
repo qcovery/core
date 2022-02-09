277e528f8e029de1ce2ede19c5e306c5d6e6cee3
kkessler <mail@kristofkessler.com>
2022-02-09 16:05:50 +0100

    test

--- module/AvailabilityPlus/src/AvailabilityPlus/Controller/AvailabilityPlusController.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Controller/AvailabilityPlusController.php
@@ -10,7 +10,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org Main Site
  */
-namespace SearchTools\Controller;
+namespace AvailabilityPlus\Controller;
 
 /**
  * AvailabilityPlus Class
