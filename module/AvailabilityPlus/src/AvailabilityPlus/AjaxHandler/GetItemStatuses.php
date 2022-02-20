1a422491cae628be15a238563ee5c752854db2da
kkessler <mail@kristofkessler.com>
2022-02-20 22:24:08 +0100

    https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/10

--- module/AvailabilityPlus/src/AvailabilityPlus/AjaxHandler/GetItemStatuses.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/AjaxHandler/GetItemStatuses.php
@@ -359,7 +359,7 @@ class GetItemStatuses extends \VuFind\AjaxHandler\GetItemStatuses implements Tra
         ];
         $response['data'] = '';
         if(!empty($resolver_url) && !empty($data)) {
-            $resolver_data = $resolverHandler->fetchLinks($params)
+            $resolver_data = $resolverHandler->fetchLinks($params);
             $response['data'] = $resolver_data['parsed_data'];
             $response['resolver_data'] = $resolver_data['data'];
             $response['test'] = $resolverHandler->test();
