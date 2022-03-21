0b706025217a1e612056abafadf3969382ddd35d
kkessler <mail@kristofkessler.com>
2022-03-21 17:41:49 +0100

    https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/65

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/DAIA.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/DAIA.php
@@ -165,7 +165,7 @@ class DAIA extends AvailabilityPlusResolver
             'doc_id' => $doc_id,
             'item_id' => $item_id
         ];
-        return $id.'/vufind/Record/'.$id.'/Hold?doc_id='.urlencode($doc_id).'&item_id='.urlencode($item_id).'&type='.$action.'&storage_id='.urlencode($storage_id).'&hashKey='.$this->hmac->generate($hmacKeys,$hmacPairs);
+        return '/vufind/Record/'.$id.'/Hold?doc_id='.urlencode($doc_id).'&item_id='.urlencode($item_id).'&type='.$action.'&storage_id='.urlencode($storage_id).'&hashKey='.$this->hmac->generate($hmacKeys,$hmacPairs);
     }
 }
 
