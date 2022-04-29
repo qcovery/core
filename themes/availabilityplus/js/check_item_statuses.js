f28631cca8687f805371436103cd593ef5f9c4b9
kkessler <mail@kristofkessler.com>
2022-04-29 11:53:40 +0200

    https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/105

--- themes/availabilityplus/js/check_item_statuses.js
+++ themes/availabilityplus/js/check_item_statuses.js
@@ -93,22 +93,6 @@ function runItemAjaxForQueue() {
                 itemStatusRunning = false;
             });
     }
-
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.display === "block") {
-                panel.style.display = "none";
-            } else {
-                panel.style.display = "block";
-            }
-        });
-    }
-
 }
 
 function itemQueueAjax(id, el) {
