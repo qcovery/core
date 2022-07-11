f685c7c3efa761e2634aa2793270aa6df9c1c692
kkessler <mail@kristofkessler.com>
2022-07-11 15:20:09 +0200

    added parsing of FulltextFinder result https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/15

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/FulltextFinder.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/FulltextFinder.php
@@ -4,6 +4,7 @@ namespace AvailabilityPlus\Resolver\Driver;
 
 class FulltextFinder extends AvailabilityPlusResolver
 {
+    protected $openUrl;
     /**
      * Get Resolver Url
      *
@@ -33,6 +34,7 @@ class FulltextFinder extends AvailabilityPlusResolver
      */
     public function fetchLinks($openUrl)
     {
+        $this->openUrl = $openUrl;
         $url = $this->getResolverUrl($openUrl);
         $password = $this->additionalParams;
         $headers = $this->httpClient->getRequest()->getHeaders();
@@ -40,6 +42,57 @@ class FulltextFinder extends AvailabilityPlusResolver
         if(!empty($password)) $headers->addHeaderLine('password', $password);
         $feed = $this->httpClient->setUri($url)->send()->getBody();
         return $feed;
+    }
+
+    /**
+     * Parse Links
+     *
+     * Parses an XML file returned by a link resolver
+     * and converts it to a standardised format for display
+     *
+     * @param string $xmlstr Raw XML returned by resolver
+     *
+     * @return array         Array of values
+     */
+    public function parseLinks($data_org)
+    {
+        $urls = []; // to check for duplicate urls
+        $records = []; // array to return
+        $data = $data_org;
+        $break = false;
+
+        if (isset($fulltextfinderApiResult->contextObjects)) {
+            foreach ($data->contextObjects as $contextObject) {
+                if (isset($contextObject->targetLinks)) {
+                    foreach ($contextObject->targetLinks as $targetLink) {
+                        if ($targetLink->category == "Fulltext") {
+                            $level = 'FulltextLeuphana';
+                            $label = 'FulltextLeuphana';
+                            $url = $targetLink;
+                            $record['level'] = $level;
+                            $record['label'] = $label;
+                            $record['url'] = $url;
+                            $records[] = $record;
+                            $break = true;
+                            break;
+                        }
+                    }
+                }
+                if($break) break;
+            }
+        }
+
+ /*       if(empty($records)) {
+            $record['level'] = $level;
+            $record['label'] = $label;
+            $record['url'] = $url;
+        }*/
+
+        $response['data'] = $data_org;
+        $this->parsed_data = $records;
+        $this->applyCustomChanges();
+        $response['parsed_data'] = $this->parsed_data;
+        return $response;
     }
 }
 
