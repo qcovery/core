cb2535f84606ab06e09a4056e9bde487d8da3d38
kkessler <mail@kristofkessler.com>
2022-07-11 15:54:29 +0200

    adjusted parsing of FulltextFinder result https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/15

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/FulltextFinder.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/FulltextFinder.php
@@ -77,7 +77,6 @@ class FulltextFinder extends AvailabilityPlusResolver
         }
 
         if(empty($links)) {
-            //"https://api.ebsco.io/ftf/ftfaccount/s2982038.main.ftf/openurl"
             $custid=substr($this->baseUrl, strpos($this->baseUrl,'ftfaccount/') +11, strlen($this->baseUrl) - strpos($this->baseUrl,'.main.ftf') - 9);
             $checkAvailabilityLink = new \stdClass();
             $checkAvailabilityLink->targetUrl = 'https://search.ebscohost.com/login.aspx?site=ftf-live&authtype=ip,guest&custid='.$custid.'&groupid=main&direct=true&'.$this->openUrl;
