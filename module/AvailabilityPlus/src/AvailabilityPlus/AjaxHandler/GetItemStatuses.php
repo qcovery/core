159b28ef7e40706b07a4df4009083dc91ffd352f
kkessler <mail@kristofkessler.com>
2022-08-27 23:01:53 +0200

    changed template for parent check
    https://gitlab.effective-webwork.de/beluga/availabilityplus/-/issues/131#note_21689

--- module/AvailabilityPlus/src/AvailabilityPlus/AjaxHandler/GetItemStatuses.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/AjaxHandler/GetItemStatuses.php
@@ -368,7 +368,7 @@ class GetItemStatuses extends \VuFind\AjaxHandler\GetItemStatuses implements Tra
     private function checkParentWorkILNSolr() {
         $check = 'checkParentWorkILNSolr';
         $check_type = 'function';
-        $template = 'ajax/link-internal.phtml';
+        $template = 'ajax/link-parent.phtml';
         $responses = [];
         $parentData = $this->driver->getMarcData('ArticleParentId');
         $response = $this->generateResponse($check, '', '', '', $template, '', '', false, $check_type);
