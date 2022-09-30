8ebcc5a3fd177a9e73450199c488656b82712345
kkessler <mail@kristofkessler.com>
2022-10-01 00:04:59 +0200

    adjusted processing of openaccess and remote services in DAIA

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/DAIA.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/DAIA.php
@@ -65,7 +65,7 @@ class DAIA extends AvailabilityPlusResolver
                                 $record->score = 0;
                                 $this->parsed_data->document[0]->item[$key]->availabilityplus = $record;
                             }
-                            //break;
+                            break;
                         case 'remote':
                             if(!in_array($service_content->href, $urls)) {
                                 $record->daia_action->level = 'LicensedAccess link_external';
@@ -82,7 +82,7 @@ class DAIA extends AvailabilityPlusResolver
                                 $record->score = 10;
                                 $this->parsed_data->document[0]->item[$key]->availabilityplus = $record;
                             }
-                            //break;
+                            break;
                         case 'loan':
                         case 'presentation':
                             if($service_key == 'loan') {
