2d9159f7583151122249da26d5dec312a019b3ec
kkessler <mail@kristofkessler.com>
2022-10-03 15:57:15 +0200

    Unpaywall: updated parsing and rendering

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/Unpaywall.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/Unpaywall.php
@@ -19,5 +19,34 @@ class Unpaywall extends AvailabilityPlusResolver
         if($doi)  $url = $this->baseUrl.'/'.$doi.'?is_oa=boolean'.$this->additionalParams;
         return $url;
     }
+
+    /**
+     * Parse Links
+     *
+     * Parses an XML file returned by a link resolver
+     * and converts it to a standardised format for display
+     *
+     * @param string $data_org JSON returned by resolver
+     *
+     * @return array         Array of values
+     */
+    public function parseLinks($data_org)
+    {
+        $urls = []; // to check for duplicate urls
+        $links = []; // array to return
+        $data = json_decode($data_org);
+
+        if(isset($data->is_oa) && $data->is_oa == true){
+            $links[0]['url'] = $data->best_oa_location->url;
+            $links[0]['label'] = 'FreeAccess';
+            $links[0]['level'] = 'FreeAccess Unpaywall';
+        }
+
+        $response['data'] = $data_org;
+        $this->parsed_data = $links;
+        $this->applyCustomChanges();
+        $response['parsed_data'] = $this->parsed_data;
+        return $response;
+    }
 }
 
