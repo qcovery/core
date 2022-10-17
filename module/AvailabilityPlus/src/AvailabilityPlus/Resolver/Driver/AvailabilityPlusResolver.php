b60df9497310c7f08bc83fddd9a9ee4e40253280
kkessler <mail@kristofkessler.com>
2022-10-17 21:14:58 +0200

    added function setLanguage to A+ Resolver

--- module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/AvailabilityPlusResolver.php
+++ module/AvailabilityPlus/src/AvailabilityPlus/Resolver/Driver/AvailabilityPlusResolver.php
@@ -25,6 +25,8 @@ class AvailabilityPlusResolver extends \VuFind\Resolver\Driver\AbstractBase
 
     protected $parsed_data;
 
+    protected $language;
+
     /**
      * Constructor
      *
@@ -204,6 +206,10 @@ class AvailabilityPlusResolver extends \VuFind\Resolver\Driver\AbstractBase
                 $this->parsed_data->item[$key][$path[0]][$path[1]][$path[2]][$path[3]][$path[4]] = $value;
                 break;
         }
+    }
+
+    public function setLanguage($language = 'en') {
+        $this->language = $language;
     }
 }
 
