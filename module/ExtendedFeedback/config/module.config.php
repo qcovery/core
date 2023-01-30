f48a14db8a06310b654dace04cb02ca9af783acd
VZG 124 <info@beluga-core.de>
2023-01-30 07:50:31 +0100

    Update VZG 124.

--- module/ExtendedFeedback/config/module.config.php
+++ module/ExtendedFeedback/config/module.config.php
@@ -5,11 +5,15 @@ $config = [
     'controllers' => [
         'factories' => [
             'ExtendedFeedback\Controller\FeedbackController' => 'VuFind\Controller\AbstractBaseFactory',
+            'ExtendedFeedback\Controller\CartController' => 'ExtendedFeedback\Controller\CartControllerFactory',
         ],
         'aliases' => [
             'Feedback' => 'ExtendedFeedback\Controller\FeedbackController',
             'feedback' => 'ExtendedFeedback\Controller\FeedbackController',
             'VuFind\Controller\FeedbackController' => 'ExtendedFeedback\Controller\FeedbackController',
+            'Cart' => 'ExtendedFeedback\Controller\CartController',
+            'cart' => 'ExtendedFeedback\Controller\CartController',
+            'VuFind\Controller\CartController' => 'ExtendedFeedback\Controller\CartController',
         ],
     ],
 ];
