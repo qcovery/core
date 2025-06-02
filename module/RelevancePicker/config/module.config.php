d74016817fa0cfd99022f4d48e7b3d3b386658ec
Johannes Schultze <schultze@effective-webwork.de>
2025-06-02 13:00:51 +0200

    Updated namespace and extended configuration for plugin managers in RelevancePicker module.

--- module/RelevancePicker/config/module.config.php
+++ module/RelevancePicker/config/module.config.php
@@ -1,5 +1,5 @@
 <?php
-namespace RelevancePicker\Module\Configuration;
+namespace RelevancePicker\Module\Config;
 
 $config = [
     'service_manager' => [
@@ -17,7 +17,36 @@ $config = [
             'VuFind\Search\Results\PluginManager' => 'RelevancePicker\Search\Results\PluginManager',
         ],
     ],
+    'vufind' => [
+        'plugin_managers' => [
+            'search_params' => [
+                'factories' => [
+                    'RelevancePicker\Search\Solr\Params' => 'RelevancePicker\Search\Solr\ParamsFactory',
+                    'RelevancePicker\Search\Search2\Params' => 'RelevancePicker\Search\Search2\ParamsFactory',
+                ],
+                'aliases' => [
+                    'solr' => 'RelevancePicker\Search\Solr\Params',
+                    'search2' => 'RelevancePicker\Search\Search2\Params',
+                ],
+            ],
+            'search_results' => [
+                'factories' => [
+                    'RelevancePicker\Search\Solr\Results' => 'RelevancePicker\Search\Solr\ResultsFactory',
+                    'RelevancePicker\Search\Search2\Results' => 'RelevancePicker\Search\Search2\ResultsFactory',
+                ],
+                'aliases' => [
+                    'solr' => 'RelevancePicker\Search\Solr\Results',
+                    'search2' => 'RelevancePicker\Search\Search2\Results',
+                ],
+            ],
+            'search_backend' => [
+                'factories' => [
+                    'Solr' => 'RelevancePicker\Search\Factory\SolrDefaultBackendFactory',
+                    'Search2' => 'RelevancePicker\Search\Factory\Search2BackendFactory',
+                ],
+            ],
+        ],
+    ],
 ];
 
 return $config;
-
